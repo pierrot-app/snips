45385d0d0246b7421cbaf65aa7cd9ef46d278f46
Leo Ternoir <leo@leoternoir.com>
2018-06-11 15:05:31 +0200

    Add support for 4mic leds

--- pixels.py
+++ pixels.py
@@ -1,126 +1,92 @@
-import apa102 as apa102
+
+import apa102
 import time
 import threading
+from gpiozero import LED
 try:
-	import queue as Queue
+    import queue as Queue
 except ImportError:
-	import Queue as Queue
+    import Queue as Queue
 
+from google_home_led_pattern import GoogleHomeLedPattern
 
 class Pixels:
-	PIXELS_N = 3
+    PIXELS_N = 12
 
-	def __init__(self):
-		self.basis = [0] * 3 * self.PIXELS_N
-		self.basis[0] = 1
-		self.basis[4] = 1
-		self.basis[8] = 2
+    def __init__(self, pattern=GoogleHomeLedPattern):
+        self.pattern = pattern(show=self.show)
 
-		self.colors = [0] * 3 * self.PIXELS_N
-		self.dev = apa102.APA102(num_led=self.PIXELS_N)
+        self.dev = apa102.APA102(num_led=self.PIXELS_N)
+        
+        self.power = LED(5)
+        self.power.on()
 
-		self.next = threading.Event()
-		self.queue = Queue.Queue()
-		self.thread = threading.Thread(target=self._run)
-		self.thread.daemon = True
-		self.thread.start()
+        self.queue = Queue.Queue()
+        self.thread = threading.Thread(target=self._run)
+        self.thread.daemon = True
+        self.thread.start()
 
-	def wakeup(self, direction=0):
-		def f():
-			self._wakeup(direction)
+        self.last_direction = None
 
-		self.next.set()
-		self.queue.put(f)
+    def wakeup(self, direction=0):
+        self.last_direction = direction
+        def f():
+            self.pattern.wakeup(direction)
 
-	def listen(self):
-		self.next.set()
-		self.queue.put(self._listen)
+        self.put(f)
 
-	def think(self):
-		self.next.set()
-		self.queue.put(self._think)
+    def listen(self):
+        if self.last_direction:
+            def f():
+                self.pattern.wakeup(self.last_direction)
+            self.put(f)
+        else:
+            self.put(self.pattern.listen)
 
-	def speak(self):
-		self.next.set()
-		self.queue.put(self._speak)
+    def think(self):
+        self.put(self.pattern.think)
 
-	def off(self):
-		self.next.set()
-		self.queue.put(self._off)
+    def speak(self):
+        self.put(self.pattern.speak)
 
-	def _run(self):
-		while True:
-			func = self.queue.get()
-			func()
+    def off(self):
+        self.put(self.pattern.off)
 
-	def _wakeup(self, direction=0):
-		for i in range(1, 25):
-			colors = [i * v for v in self.basis]
-			self.write(colors)
-			time.sleep(0.01)
+    def put(self, func):
+        self.pattern.stop = True
+        self.queue.put(func)
 
-		self.colors = colors
+    def _run(self):
+        while True:
+            func = self.queue.get()
+            self.pattern.stop = False
+            func()
 
-	def _listen(self):
-		for i in range(1, 25):
-			colors = [i * v for v in self.basis]
-			self.write(colors)
-			time.sleep(0.01)
+    def show(self, data):
+        for i in range(self.PIXELS_N):
+            self.dev.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
 
-		self.colors = colors
-
-	def _think(self):
-		colors = self.colors
-
-		self.next.clear()
-		while not self.next.is_set():
-			colors = colors[3:] + colors[:3]
-			self.write(colors)
-			time.sleep(0.2)
-
-		t = 0.1
-		for i in range(0, 5):
-			colors = colors[3:] + colors[:3]
-			self.write([(v * (4 - i) / 4) for v in colors])
-			time.sleep(t)
-			t /= 2
-
-		# time.sleep(0.5)
-
-		self.colors = colors
-
-	def _speak(self):
-		colors = self.colors
-
-		self.next.clear()
-		while not self.next.is_set():
-			for i in range(5, 25):
-				self.write([(v * i / 24) for v in colors])
-				time.sleep(0.01)
-
-			time.sleep(0.3)
-
-			for i in range(24, 4, -1):
-				self.write([(v * i / 24) for v in colors])
-				time.sleep(0.01)
-
-			time.sleep(0.3)
-
-		self._off()
-
-	def _off(self):
-		self.write([0] * 3 * self.PIXELS_N)
-
-	def write(self, colors):
-		for i in range(self.PIXELS_N):
-			self.dev.set_pixel(i, int(colors[3*i]), int(colors[3*i + 1]), int(colors[3*i + 2]))
-
-		self.dev.show()
+        self.dev.show()
 
 
-instance = None
-def getInstance():
-	global instance
-	if instance is None:
-		instance = Pixels()
-	return instance
+pixels = Pixels()
+
+
+if __name__ == '__main__':
+    while True:
+
+        try:
+            pixels.wakeup()
+            time.sleep(3)
+            pixels.think()
+            time.sleep(3)
+            pixels.speak()
+            time.sleep(6)
+            pixels.off()
+            time.sleep(3)
+        except KeyboardInterrupt:
+            break
+
+
+    pixels.off()
+    time.sleep(1)
